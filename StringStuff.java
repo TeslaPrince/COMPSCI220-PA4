095a3b7cc290a72f729a045deaf4789b1dfc0723
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2020-10-04 13:40:29 -0500

    Update StringStuff.java

--- StringStuff.java
+++ StringStuff.java
@@ -1,17 +1,46 @@
 package PA4;
 
+import java.util.Arrays;
+
 public class StringStuff {
-	public static boolean isPalindrome() {
+	public static boolean isPalindrome(String str) {
+		int a = 0;
+		int b = str.length() - 1;
+		while (a < b) {
+			if (str.charAt(a) != str.charAt(b)) {
+				return false;
+			}
+			a++;
+			b--;
+		}
 		return true;
 	}
-	public static String longestPalindromWithin() {
-		return "";
+	public static String longestPalindromeWithin(String str) {
+		String longestSub = "";
+		return longestSub;
 	}
-	public static boolean containsAllDigits() {
+	public static boolean containsAllDigits(String str) {
+		boolean[] contains09 = {false, false, false, false, false, false, false, false, false, false};
+		for(int i = 0; i < str.length(); i++) {
+			int a = str.charAt(i);
+			if (a >= 48 && a <= 57) {
+				char b = (char)a;
+				int c = Character.getNumericValue(b);
+				contains09[c] = true;
+			}
+		}
+		for(int i = 0; i < 10; i++) {
+			if (!contains09[i]) {
+				return false;
+			}
+		}
 		return true;
 	}
-	public static String replaceAll() {
+	/*public static String replaceAll(String text, String key, String replacement) {
+		
 		return"";
 	}
-	public
+	public static String runLengthEncode(String str) {
+		return "";
+	}*/
 }
