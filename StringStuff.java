68852b24016a6430b1e002363657736bf6b42dd2
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2020-10-04 11:00:15 -0500

    Create StringStuff.java

---
+++ StringStuff.java
@@ -0,0 +1,17 @@
+package PA4;
+
+public class StringStuff {
+	public static boolean isPalindrome() {
+		return true;
+	}
+	public static String longestPalindromWithin() {
+		return "";
+	}
+	public static boolean containsAllDigits() {
+		return true;
+	}
+	public static String replaceAll() {
+		return"";
+	}
+	public
+}
